279fb9c90e9258fc90f4508749efd8636257a90c
Mohammed Aslam <mohammedaslam1219@gmail.com>
2024-07-22 16:58:31 +0530

    Responsive - commit

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './navbar.css'
 import logo from '../Assets/logo.png'
 import cart from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
 const Navbar = () => {
 
-  const [menu, setMenu] = useState("Shop")
+  const [menu, setMenu] = useState("Shop");
   const {getTotalCartItem} = useContext(ShopContext);
+  const menuRef = useRef();
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
@@ -16,7 +22,8 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
             <p>Shopify</p>
         </div>
-        <ul className="nav-menu">
+        <img className='nav_dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none', color: 'inherit'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration: 'none', color: 'inherit'}} to='/men'>Men</Link> {menu==="men"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration: 'none', color: 'inherit'}} to='/women'>Women</Link> {menu==="women"?<hr/>:<></>}</li>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product';
 
 
